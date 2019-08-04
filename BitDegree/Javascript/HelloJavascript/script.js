0940250d2d60be121e4da873518fd0d308165cfd
zohzik <theynel3@gmail.com>
2019-08-04 08:38:50 -0500

    Working with javascript strings and variables

---
+++ BitDegree/Javascript/HelloJavascript/script.js
@@ -0,0 +1,18 @@
+// var name = "jack";
+// var age = 23;
+// var shootingScore = 45.6;
+//
+// var message = "Hi, my name is " + name = " and I am " + age + "years old.";
+//
+// console.log(message);
+
+
+var firstName = "John";
+var lastName = "JacobJingleHeimerSmith";
+var dateOfBirth = "10-09-82";
+var age = 23;
+var profileImgUrl = "http://coolpicks.com/johnjacob.jpg";
+
+var loginWelcomeMsg = "Welcome, " + firstName + ". Happy " + age + "rd Birthday!";
+
+console.log(loginWelcomeMsg);
