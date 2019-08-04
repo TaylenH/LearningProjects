a08236ab5c33437115e86db70a977c413da18fc4
zohzik <theynel3@gmail.com>
2019-08-04 08:51:27 -0500

    working with numbers and operators

--- BitDegree/Javascript/HelloJavascript/script.js
+++ BitDegree/Javascript/HelloJavascript/script.js
@@ -7,12 +7,23 @@
 // console.log(message);
 
 
-var firstName = "John";
-var lastName = "JacobJingleHeimerSmith";
-var dateOfBirth = "10-09-82";
-var age = 23;
-var profileImgUrl = "http://coolpicks.com/johnjacob.jpg";
+// var firstName = "John";
+// var lastName = "JacobJingleHeimerSmith";
+// var dateOfBirth = "10-09-82";
+// var age = 23;
+// var profileImgUrl = "http://coolpicks.com/johnjacob.jpg";
+//
+// var loginWelcomeMsg = "Welcome, " + firstName + ". Happy " + age + "rd Birthday!";
+//
+// console.log(loginWelcomeMsg);
 
-var loginWelcomeMsg = "Welcome, " + firstName + ". Happy " + age + "rd Birthday!";
+var sum = 10 + 15;
+var sub = 15 - 10;
+var mul = 10 * 3;
+var div = 9 / 3;
+var mod = 10 % 3;
 
-console.log(loginWelcomeMsg);
+var result = 10 * ((5 + 3) - 4);
+
+
+console.log(sub);
