62c8b792296800b8ffcafb79bc749d717e4ab50a
zohzik <theynel3@gmail.com>
2019-08-18 09:05:37 -0500

    working with objects

--- BitDegree/Javascript/HelloJavascript/script.js
+++ BitDegree/Javascript/HelloJavascript/script.js
@@ -1,3 +1,4 @@
+"use strict";
 // var name = "jack";
 // var age = 23;
 // var shootingScore = 45.6;
@@ -89,37 +90,107 @@
 // var width2 = 14;
 // var area2 = length2 + width2;
 
-function area(length, width){
-  return length * width;
-}
+// function area(length, width){
+//   return length * width;
+// }
+//
+// var area1 = area(10, 15);
+// console.log(area1);
+//
+// var rectangles = [];
+// rectangles.push(area(10, 15));
+// rectangles.push(area(14, 2));
+// rectangles.push(area(4, 5));
+// console.log(rectangles);
+//
+// var bankBalance = 500;
+//
+// function makeTransaction(salePrice){
+//   if(salePrice <= bankBalance){
+//     bankBalance -= salePrice;
+//     console.log("purchase successful");
+//   }else{
+//     console.log("Insufficient Funds");
+//   }
+// }
+//
+// console.log(bankBalance);
+// makeTransaction(79.00);
+// console.log(bankBalance);
+// makeTransaction(2.32);
+// console.log(bankBalance);
+// makeTransaction(10.45);
+// console.log(bankBalance);
+//
+// makeTransaction(450.00);
+// console.log(bankBalance);
 
-var area1 = area(10, 15);
-console.log(area1);
+var students = [];
 
-var rectangles = [];
-rectangles.push(area(10, 15));
-rectangles.push(area(14, 2));
-rectangles.push(area(4, 5));
-console.log(rectangles);
+// function Student(firstName, lastName, age) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.greeting = ()=>`HI im ${this.firstName} and im ${this.age} years old.`;
+// }
 
-var bankBalance = 500;
-
-function makeTransaction(salePrice){
-  if(salePrice <= bankBalance){
-    bankBalance -= salePrice;
-    console.log("purchase successful");
-  }else{
-    console.log("Insufficient Funds");
+class Student{
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
   }
+
+  greeting = ()=>`Hi, im ${this.firstName} and im ${this.age} years old`;
 }
 
-console.log(bankBalance);
-makeTransaction(79.00);
-console.log(bankBalance);
-makeTransaction(2.32);
-console.log(bankBalance);
-makeTransaction(10.45);
-console.log(bankBalance);
+students.push(new Student("Jenny", "laga", 5));
+students.push(new Student("timmy", "turner", 8));
+students.push(new Student("carl", "jr", 4));
 
-makeTransaction(450.00);
-console.log(bankBalance);
+for(var i = 0; i < students.length; i++)
+  console.log(students[i].greeting());
+
+for(let e in students[0])
+  console.log(students[0][e])
+
+function doStuff(){
+  console.log("stuff");
+}
+
+// var student0 = {
+//   firstName: "Jaye",
+//   lastName: "Lou",
+//   age: 8
+// };
+//
+// console.log(student0.greeting());
+// //creates a new empty object
+// var student1 = {};
+// student1.firstName = "john";
+// student1.lastName = "parker";
+// student1.age = 7;
+//
+// var student2 = new Object();
+// student2.firstName = "Zac";
+// student2.lastName = "bobo";
+// student2.age = 5;
+//
+// // console.log(student.firstName);
+// // console.log(student.lastName);
+// // console.log(student["firstName"]);
+// // console.log(student["age"]);
+// students.push(student0);
+// students.push(student1);
+// students.push(student2);
+// console.log(students);
+
+
+var makeStuff = (function(){
+  var x = 0;
+  return function () {
+    x++;
+    console.log(x);
+    return true;
+  };
+})();
