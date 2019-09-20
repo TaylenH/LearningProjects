018da19db19113d8dd7013d5b898ce234e3c97e3
zohzik <theynel3@gmail.com>
2019-09-20 16:29:58 -0500

    added put and delete

--- BitDegree/nodejs/firstNodeAPI/server.js
+++ BitDegree/nodejs/firstNodeAPI/server.js
@@ -25,11 +25,11 @@ var ingredients = [{
   }
 ];
 
-app.get('/', function(request, response) {
+app.get('/ingredients', function(request, response) {
   response.send(ingredients);
 });
 
-app.post('/', function(req, res) {
+app.post('/ingredients', function(req, res) {
   var ingredient = req.body;
   if (!ingredient || ingredient.text === "") {
     res.status(500).send({
@@ -40,6 +40,57 @@ app.post('/', function(req, res) {
     res.status(200).send(ingredient);
   }
 });
+
+app.put('/ingredients/:ingredientId', function(req, res) {
+  var ingredientId = req.params.ingredientId;
+  var text = req.body.text;
+  var found = false;
+
+  if (!text || text === "") {
+    res.status(500).send({
+      error: "You must a provide ingredient"
+    })
+  } else {
+    for (var x = 0; x < ingredients.length; x++) {
+      var ing = ingredients[x];
+
+      if (ing.id === req.params.ingredientId) {
+        ingredients[x].text = text;
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      res.status(500).send({
+        error: "Ingredient does not exist"
+      });
+      return;
+    }
+
+    res.send(ingredients);
+  }
+});
+
+app.delete('/ingredients/:ingredientId', function(req, res) {
+  var id = req.params.ingredientId;
+  var success = false;
+
+  for (var i = 0; i < ingredients.length; i++) {
+    if (id === ingredients[i].id) {
+      ingredients.splice(i, 1);
+      success = true;
+    }
+  }
+
+  if (success) {
+    res.send(ingredients);
+  } else {
+    res.status(500).send({
+      error: "Ingredient id not found"
+    });
+  }
+})
 
 app.listen(3000, function() {
   console.log("Fisrt api running on port 3000");
