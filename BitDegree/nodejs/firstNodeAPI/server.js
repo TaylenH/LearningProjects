45ce108b10003d40b252debdf097a38d8f9cdced
zohzik <theynel3@gmail.com>
2019-09-19 19:26:42 -0500

    added post request handler

--- BitDegree/nodejs/firstNodeAPI/server.js
+++ BitDegree/nodejs/firstNodeAPI/server.js
@@ -1,12 +1,44 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var ingredients = [{
+    'id': "2343kak",
+    'text': "eggs"
+  },
+  {
+    "id": "dkp345",
+    "text": "milk"
+  },
+  {
+    "id": "dkcuu7",
+    "text": "bacon"
+  },
+  {
+    "id": "73hdy",
+    "text": "frogs legs"
+  }
+];
+
 app.get('/', function(request, response) {
-  response.send('My First API!');
+  response.send(ingredients);
 });
 
-app.get('/funions', function(req, res) {
-  res.send("Give me them funions");
+app.post('/', function(req, res) {
+  var ingredient = req.body;
+  if (!ingredient || ingredient.text === "") {
+    res.status(500).send({
+      error: "Your ingredient must have text"
+    });
+  } else {
+    ingredients.push(ingredient);
+    res.status(200).send(ingredient);
+  }
 });
 
 app.listen(3000, function() {
